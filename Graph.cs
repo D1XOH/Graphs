a758ed31e3855af2a590d2548f45f5e1865ecf39
D1XOH <70796382+D1XOH@users.noreply.github.com>
2022-09-19 18:04:08 +0700

    Add files via upload

---
+++ Graph.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace kortej
+{
+
+    class Graph
+    {
+        private List<(string First, string Second, int Distance)> cities;
+        private List<string> citiesNames;
+
+        public Graph(List<(string First, string Second, int Distance)> cities)
+        {
+            citiesNames = new List<string>();
+            this.cities = cities;
+
+            foreach (var (First, Second, Distance) in cities)
+            {
+                if (!citiesNames.Contains(First))
+                {
+                    citiesNames.Add(First);
+                }
+                if(!citiesNames.Contains(Second))
+                {
+                    citiesNames.Add(Second);
+                }
+            }
+        }
+        public string[] GetNamesCities()
+        {
+            return citiesNames.ToArray();
+        }
+        public int FindMin()
+        {
+            int min = Int32.MaxValue;
+            foreach (var (First, Second, Distance) in cities)
+            {
+                if (Distance < min)
+                {
+                    min = Distance;
+                }
+            }
+            return min;
+        }
+        public int FindMax()
+        {
+            int max = Int32.MinValue;
+            foreach (var (First, Second, Distance) in cities)
+            {
+                if (Distance > max)
+                {
+                    max = Distance;
+                }
+            }
+            return max;
+        }
+        public int Sum()
+        {
+            int sum = 0;
+            foreach (var (First, Second, Distance) in cities)
+            {
+                sum += Distance;
+            }
+            sum /= 2;
+            return sum;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace kortej
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            List<(string, string, int)> cities = new List<(string, string, int)>
+            {
+                ("A", "B", 5),
+                ("B", "A", 5),
+                ("A", "D", 9),
+                ("D", "A", 9),
+                ("D", "C", 7),
+                ("C", "D", 7),
+                ("D", "E", 6),
+                ("E", "D", 6),
+                ("C", "E", 8),
+                ("E", "C", 8),
+                ("B", "E", 9),
+                ("E", "B", 9),
+            };
+            Graph graph = new Graph(cities);
+
+            foreach(var i in graph.GetNamesCities())
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+            int min = graph.FindMin();
+            Console.WriteLine("Min distance {0}", min);
+            int max = graph.FindMax();
+            Console.WriteLine("Max distance {0}", max);
+            int sum = graph.Sum();
+            Console.WriteLine("Sum all of distance {0}", sum);
+
+
+        }
+    }
+}
